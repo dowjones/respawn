57ed6c93d1cb0c91f204e52ae1f4df197301954c
Vick Mena <vickmena@gmail.com>
2015-03-20 12:46:18 -0400

    Allow optional validation off by default

--- gen.sh
+++ gen.sh
@@ -7,6 +7,25 @@ if [ -z ${VIRTUAL_ENV} ]; then
 	source ${tmpdir}/bin/activate
 fi
 
+validate=0
+OPTIND=1
+while getopts ":v" arg; do
+  case $arg in
+    v)
+		validate=1
+		;;
+    \?)
+		echo "Invalid option: -$OPTARG" >&2
+		exit 1
+		;;
+    :)
+		echo "Option -$OPTARG requires an argument." >&2
+		exit 2
+		;;
+  esac
+done
+shift $((OPTIND-1))
+
 StackRole=${1:-dev}
 
 # Version control
@@ -24,7 +43,7 @@ PATCH=$((PATCH+1))
 ) > version.yaml
 
 # Install deps
-pip install -r py_reqs.txt || exit -2
+pip install -r py_reqs.txt || exit -3
 
 # Build all of the required templates
 for opt in ${StackRole}/*.yaml; do
@@ -38,11 +57,13 @@ for opt in ${StackRole}/*.yaml; do
 	cfn_py_generate gen.py -o ${opt} &> ${cftName}
 
 	# Validate the CFT
-	echo "Validating ${cftName}..."
-	aws --region us-east-1 cloudformation validate-template --template-body file://${cftName}
-	if [ $? != 0 ]; then
-		echo "Validation FAILED"
-		exit -1
+	if [[ ${validate} -gt 0 ]]; then
+		echo "Validating ${cftName}..."
+		aws --region us-east-1 cloudformation validate-template --template-body file://${cftName}
+		if [ $? != 0 ]; then
+			echo "Validation FAILED"
+			exit -4
+		fi
 	fi
 	echo "Success"
 	echo
